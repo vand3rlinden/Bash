5e11f2ec119b57de47fd0108e46d86091d6be577
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2024-09-22 12:37:47 +0200

    Update sendmail.sh

--- sendmail.sh
+++ sendmail.sh
@@ -14,5 +14,5 @@ body="Hi Ricardo,\n\nThis is a test email.\n\nBest regards,\nSomeone"
 # Send the email using mail command from mailutils
 echo -e "$headers\n$body" | mail -a "From: $sender" -a "Return-Path: $return_path_sender" "$recipient" -s "$subject"
 
-#You can change the P1 sender domain by modifying the return_path_sender line for the testing DMARC-compliant for the P2 sender or testing out SPF macro's for your domain.
+#You can change the P1 sender domain by modifying the return_path_sender line for the testing DMARC-compliant for the P2 sender.
 #Prerequisites: Postfix (sudo apt-get install postfix) and Mailutils (sudo apt-get install mailutils)
