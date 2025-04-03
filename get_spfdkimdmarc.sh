bc72df770da2d5a813469067a5d9a1debf35d079
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2025-04-03 13:31:31 +0200

    Update get_spfdkimdmarc.sh

--- get_spfdkimdmarc.sh
+++ get_spfdkimdmarc.sh
@@ -13,14 +13,25 @@ get_spf_record() {
 }
 
 # Function to retrieve DMARC record for a domain
+# Function to retrieve DMARC record for a domain
 get_dmarc_record() {
     domain="$1"
     dmarc_record=$(dig "_dmarc.$domain" TXT +short)
     if [ -n "$dmarc_record" ]; then
         echo -e "\033[32mDMARC record for $domain:\033[0m"
         echo "$dmarc_record"
+
+        # Check for DMARC policy
+        if echo "$dmarc_record" | grep -q 'p=reject'; then
+            echo -e "\033[32mDomain is DMARC compliant — the domain is protected against abuse.\033[0m"
+        elif echo "$dmarc_record" | grep -q 'p=quarantine'; then
+            echo -e "\033[33mDomain is partially DMARC compliant — the domain is somewhat protected against abuse.\033[0m"
+        else
+            echo -e "\033[31mDomain is not DMARC compliant — the domain is not protected against abuse.\033[0m"
+        fi
     else
-        echo -e "\033[32mNo DMARC record found for $domain\033[0m"
+        echo -e "\033[31mNo DMARC record found for $domain\033[0m"
+        echo -e "\033[31mDomain is not DMARC compliant — the domain is not protected against abuse.\033[0m"
     fi
 }
 
