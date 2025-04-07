056c436a75eb1263618b55b5c7bd67c740e84eaa
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2025-04-07 18:07:59 +0200

    Update get_spfdkimdmarc.sh

--- get_spfdkimdmarc.sh
+++ get_spfdkimdmarc.sh
@@ -12,28 +12,6 @@ get_spf_record() {
     fi
 }
 
-# Function to retrieve DMARC record for a domain
-get_dmarc_record() {
-    domain="$1"
-    dmarc_record=$(dig "_dmarc.$domain" TXT +short)
-    if [ -n "$dmarc_record" ]; then
-        echo -e "\033[32mDMARC record for $domain:\033[0m"
-        echo "$dmarc_record"
-
-        # Check for DMARC policy
-        if echo "$dmarc_record" | grep -q 'p=reject'; then
-            echo -e "\033[32mDomain is DMARC compliant — the domain is protected against abuse.\033[0m"
-        elif echo "$dmarc_record" | grep -q 'p=quarantine'; then
-            echo -e "\033[33mDomain is partially DMARC compliant — the domain is somewhat protected against abuse.\033[0m"
-        else
-            echo -e "\033[31mDomain is not DMARC compliant — the domain is not protected against abuse.\033[0m"
-        fi
-    else
-        echo -e "\033[31mNo DMARC record found for $domain\033[0m"
-        echo -e "\033[31mDomain is not DMARC compliant — the domain is not protected against abuse.\033[0m"
-    fi
-}
-
 # Function to retrieve DKIM record for a domain
 get_dkim_record() {
     domain="$1"
@@ -63,6 +41,28 @@ else
     selector="$2"
 fi
 
+# Function to retrieve DMARC record for a domain
+get_dmarc_record() {
+    domain="$1"
+    dmarc_record=$(dig "_dmarc.$domain" TXT +short)
+    if [ -n "$dmarc_record" ]; then
+        echo -e "\033[32mDMARC record for $domain:\033[0m"
+        echo "$dmarc_record"
+
+        # Check for DMARC policy
+        if echo "$dmarc_record" | grep -q 'p=reject'; then
+            echo -e "\033[32mDomain is DMARC compliant — the domain is protected against abuse.\033[0m"
+        elif echo "$dmarc_record" | grep -q 'p=quarantine'; then
+            echo -e "\033[33mDomain is partially DMARC compliant — the domain is somewhat protected against abuse.\033[0m"
+        else
+            echo -e "\033[31mDomain is not DMARC compliant — the domain is not protected against abuse.\033[0m"
+        fi
+    else
+        echo -e "\033[31mNo DMARC record found for $domain\033[0m"
+        echo -e "\033[31mDomain is not DMARC compliant — the domain is not protected against abuse.\033[0m"
+    fi
+}
+
 get_spf_record "$domain"
-get_dmarc_record "$domain"
 get_dkim_record "$domain" "$selector"
+get_dmarc_record "$domain"
